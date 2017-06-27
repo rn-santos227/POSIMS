5f93e62b270b7272797b97f71fec84c02b8d1b97
zsanagustin <zsanagustin4@gmail.com>
2017-06-27 11:29:49 +0800

    c

--- back/resources/views/welcome.blade.php
+++ back/resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel#
+                    Laravelasd
                 </div>
 
                 <div class="links">
