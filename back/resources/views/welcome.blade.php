eb1473d55cd4c06688f1059c26af61a18b48a61b
zsanagustin <zsanagustin4@gmail.com>
2017-06-27 11:37:12 +0800

    commit 3
    dinagdagan ng s

--- back/resources/views/welcome.blade.php
+++ back/resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravelasd
+                    Laravelasds
                 </div>
 
                 <div class="links">
